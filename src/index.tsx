ca5e64fd18bed6fc2c1c7d42721e7bcdf7c1fda6
maxyma-sanx <maxymasan.dev@gmail.com>
2023-08-28 15:45:40 +0300

    add fonts and normalize

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 
 import { App } from './App';
 import { ThemeProvider } from 'styled-components';
-import { theme } from './styles';
+import { GlobalStyle, theme } from './styles';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
@@ -12,6 +12,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
+      <GlobalStyle />
       <App />
     </ThemeProvider>
   </React.StrictMode>,
