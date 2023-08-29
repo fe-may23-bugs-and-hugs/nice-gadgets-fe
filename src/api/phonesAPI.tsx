33e9aa4b33a7f933bc7038a2d853ce7861bb324c
Anton Baranskyi <antonbaranskij12@gmail.com>
2023-08-29 14:16:51 +0300

    Add types

--- src/api/phonesAPI.tsx
+++ src/api/phonesAPI.tsx
@@ -1,9 +1,11 @@
 import { client } from '../helpers/fetchClient';
+import { Phone } from '../types/Phone';
+import { PhoneDetails } from '../types/PhoneDetails';
 
 export const getPhones = () => {
-  return client.get('../assets/data/phones.json'); // mock url
+  return client.get<Phone[]>('../assets/data/phones.json'); // mock url
 };
 
 export const getOnePhone = (phoneId: string) => {
-  return client.get(`../assets/data/phones/${phoneId}.json`); // mock url
+  return client.get<PhoneDetails>(`../assets/data/phones/${phoneId}.json`); // mock url
 };
