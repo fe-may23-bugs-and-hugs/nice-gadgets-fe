8257818fb2f969179587fdca217afbadd6e3b451
Serhii Melnyk <serzh2001melnyk@gmail.com>
2023-08-31 14:16:17 +0300

    fixed markup CardPage

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
 import React from 'react';
-// import { Outlet } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 import { AppLayout } from './module/shared/AppLayout/AppLayout';
 import { Header, Footer } from './module/shared';
-import { CartPage } from './module';
 
 export const App = () => (
   <>
     <Header />
     <AppLayout>
-      <CartPage />
-      {/* <Outlet /> */}
+      <Outlet />
     </AppLayout>
     <Footer />
   </>
