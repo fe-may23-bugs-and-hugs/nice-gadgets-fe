6ea111f219c477a66c459c57e3501c773352f5ab
Zoriana Melnychuk <zoryashka3002@gmail.com>
2023-09-06 16:33:29 +0200

    add LoginPage first version

---
+++ src/module/LoginPage/components/LoginForm/LoginForm.styled.tsx
@@ -0,0 +1,114 @@
+import { styled } from 'styled-components';
+import { onDesktop, onTablet, pageGridMixin } from '../../../shared/Mixins';
+import { Link } from 'react-router-dom';
+
+export const SectionWrapper = styled.div`
+  margin-top: 57px;
+  margin-bottom: 57px;
+  display: grid;
+  ${pageGridMixin}
+  color: ${({ theme }) => theme.colors.grayPrimary};
+  font-size: ${({ theme }) => theme.fonts.sizeS};
+  font-weight: ${({ theme }) => theme.fonts.weightRegular};
+  font-style: normal;
+  line-height: ${({ theme }) => theme.fonts.lineHeightXl};
+`;
+
+export const FormWrapper = styled.div`
+  grid-column: 1 / -1;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 16px;
+
+  ${onTablet(`
+    grid-column: 3 / -3;
+    gap: 24px;
+  `)}
+
+  ${onDesktop(`
+    grid-column: 5 / -5;
+  `)}
+`;
+
+export const Form = styled.form`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+  align-items: flex-start;
+
+  ${onTablet(`
+    gap: 24px;
+  `)}
+`;
+
+export const InputWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 4px;
+`;
+
+export const SubmitButton = styled.button`
+  width: 100%;
+  height: 40px;
+  text-align: center;
+  cursor: pointer;
+  border-radius: 8px;
+  background: ${(props) =>
+    props.theme.colors.accentPrimary};
+  color: ${(props) => props.theme.colors.white};
+  border: none;
+  transition: all .3s;
+
+  font-size: ${({ theme }) => theme.fonts.sizeXs};
+  font-weight: ${({ theme }) => theme.fonts.weightRegular};
+  line-height: ${({ theme }) => theme.fonts.lineHeightM};
+
+  ${onTablet(`
+    height: 50px;
+  `)}
+
+  &:hover {
+    box-shadow: 0px 3px 13px 0px rgba(23, 32, 49, 0.4);
+  }
+
+  &:active {
+    background: ${(props) => props.theme.colors.white};
+    color: ${(props) => props.theme.colors.accentPrimary};
+    border: 1px solid ${(props) => props.theme.colors.grayElements};
+  }
+`;
+
+export const Label = styled.label`
+  color: ${(props) =>
+    props.theme.colors.grayPrimary};
+`;
+
+export const Input = styled.input`
+  height: 40px;
+  border-radius: 8px;
+  border: 1px solid gray;
+  text-indent: 4px;
+
+  ${onTablet(`
+    height: 50px;
+  `)}
+
+  &::placeholder {
+    font-size: ${({ theme }) => theme.fonts.sizeXs};
+    color: ${(props) =>
+    props.theme.colors.graySecondary};
+  }
+`;
+
+export const FormLink = styled(Link)`
+  font-size: ${({ theme }) => theme.fonts.sizeXs};
+  line-height: ${({ theme }) => theme.fonts.lineHeightM};
+
+  &:hover {
+    color: ${(props) =>
+    props.theme.colors.graySecondary};
+  }
+`;
---
+++ src/module/LoginPage/components/LoginForm/LoginForm.tsx
@@ -0,0 +1,127 @@
+import React, { useState } from 'react';
+import {
+  SectionWrapper,
+  Form,
+  InputWrapper,
+  SubmitButton,
+  Label,
+  Input,
+  FormWrapper,
+  FormLink,
+} from './LoginForm.styled';
+import { useLocation } from 'react-router-dom';
+
+// enum FORMTYPES {
+//   LOGIN = 'Log In',
+//   SIGNUP = 'Sign Up',
+// }
+
+const formTypeData = [
+  {
+    type: 'Log In',
+    path: '/logIn',
+  },
+  {
+    type: 'Sign Up',
+    path: '/signUp',
+  },
+];
+
+export const LoginForm = () => {
+  const currentLocation = useLocation();
+  const currentForm = formTypeData
+    .find((form) => form.path === currentLocation.pathname);
+  const otherForm = formTypeData
+    .find((form) => form.path !== currentLocation.pathname);
+
+  const [userName, setUserName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const [userNameError, setUserNameError] = useState(false);
+  const [emailError, setEmailError] = useState(false);
+  const [passwordError, setPasswordError] = useState(false);
+
+  const handleNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setUserName(e.target.value);
+  };
+
+  const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value);
+  };
+
+  const handleSubmit = (e: React.ChangeEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    setUserNameError(currentForm?.type === 'Sign Up' && !userName);
+    setEmailError(!email);
+    setPasswordError(!password);
+
+    if (userNameError || emailError || passwordError) {
+      return;
+    }
+
+    setUserName('');
+    setEmail('');
+    setPassword('');
+  };
+
+  return (
+    <SectionWrapper>
+      <FormWrapper>
+        <h2>{currentForm ? currentForm.type : 'Log In'}</h2>
+        <Form onSubmit={handleSubmit}>
+          {
+            currentForm?.type === 'Sign Up'
+            && (
+              <InputWrapper>
+                <Label htmlFor="name">Name</Label>
+                <Input
+                  type="text"
+                  id="name"
+                  value={userName}
+                  onChange={handleNameChange}
+                  placeholder="Enter name"
+                  required
+                />
+              </InputWrapper>
+            )
+          }
+          <InputWrapper>
+            <Label htmlFor="email">Email</Label>
+            <Input
+              type="email"
+              id="email"
+              value={email}
+              onChange={handleEmailChange}
+              placeholder="Enter email"
+              required
+            />
+          </InputWrapper>
+          <InputWrapper>
+            <Label htmlFor="password">Password</Label>
+            <Input
+              type="password"
+              id="password"
+              value={password}
+              onChange={handlePasswordChange}
+              placeholder="Enter password"
+              required
+            />
+          </InputWrapper>
+          <SubmitButton type="submit">
+            {currentForm ? currentForm.type : 'Log In'}
+          </SubmitButton>
+        </Form>
+        <FormLink to={otherForm?.path || '/signUp'} >
+          {otherForm?.type || 'Sign Up'}
+        </FormLink>
+
+      </FormWrapper>
+    </SectionWrapper>
+  );
+};
