849254a6ae2745104c0ca1893e200283fc3eb5fa
Ihor Oksentiuk <winbka.lem@gmail.com>
2023-09-07 22:38:02 +0300

    Fixed skeleton

--- src/module/shared/PhoneCard/PhoneCard.tsx
+++ src/module/shared/PhoneCard/PhoneCard.tsx
@@ -3,8 +3,6 @@
 /* eslint-disable max-len */
 import React, { useContext } from 'react';
 import { useTheme } from 'styled-components';
-import ContentLoader from 'react-content-loader';
-
 import {
   CardImage,
   CardTitle,
@@ -24,7 +22,7 @@ import {
 
 import { Icon, IconSprite } from '../Sprites';
 import { Phone } from '../../../types/Phone';
-import { FavoriteContext, CartContext, PhonesContext, useTheme as useCustomTheme } from '../../../context';
+import { FavoriteContext, CartContext, useTheme as useCustomTheme } from '../../../context';
 
 type Props = {
   phone: Phone;
@@ -35,8 +33,6 @@ export const PhoneCard: React.FC<Props> = ({ phone }) => {
   const { isDarkTheme } = useCustomTheme() || { isDarkTheme: false };
 
   const { addItem, cartProducts } = useContext(CartContext);
-  const { phonesLoading, newLoader, discountLoader } =
-    useContext(PhonesContext);
 
   const { addFavoriteProduct, favoriteProducts } = useContext(FavoriteContext);
 
@@ -59,95 +55,72 @@ export const PhoneCard: React.FC<Props> = ({ phone }) => {
 
   return (
     <CardWrapper isDarkTheme={isDarkTheme}>
-      {phonesLoading || newLoader || discountLoader ? (
-        <>
-          <ContentLoader
-            speed={2}
-            width={206}
-            height={453}
-            viewBox="0 0 206 453"
-            backgroundColor="#f3f3f3"
-            foregroundColor="#ecebeb"
-          >
-            <rect x="0" y="0" rx="0" ry="0" width="208" height="208" />
-            <rect x="0" y="228" rx="0" ry="0" width="206" height="21" />
-            <rect x="0" y="268" rx="0" ry="0" width="206" height="40" />
-            <rect x="0" y="327" rx="0" ry="0" width="206" height="67" />
-            <rect x="0" y="408" rx="0" ry="0" width="206" height="40" />
-          </ContentLoader>
-        </>
-      ) : (
-        // eslint-disable-next-line indent
-        <>
-          <ImageBox to={fullPath}>
-            <CardImage src={phone.images[0]} alt="Phone Image" />
-          </ImageBox>
-          <CardTitle to={phone._id} isDarkTheme={isDarkTheme}>{phone.name}</CardTitle>
+      <ImageBox to={fullPath}>
+        <CardImage src={phone.images[0]} alt="Phone Image" />
+      </ImageBox>
+      <CardTitle to={phone._id} isDarkTheme={isDarkTheme}>{phone.name}</CardTitle>
 
-          <PriceWrapper isDarkTheme={isDarkTheme}>
-            <CurrentPrice isDarkTheme={isDarkTheme}>{`$${phone.priceDiscount}`}</CurrentPrice>
-            <OldPrice isDarkTheme={isDarkTheme}>{`$${phone.priceRegular}`}</OldPrice>
-          </PriceWrapper>
+      <PriceWrapper isDarkTheme={isDarkTheme}>
+        <CurrentPrice isDarkTheme={isDarkTheme}>{`$${phone.priceDiscount}`}</CurrentPrice>
+        <OldPrice isDarkTheme={isDarkTheme}>{`$${phone.priceRegular}`}</OldPrice>
+      </PriceWrapper>
 
-          <DescrWrapper>
-            <DescrBox>
-              <DescrTitle isDarkTheme={isDarkTheme}>Screen</DescrTitle>
-              <DescrTitle isDarkTheme={isDarkTheme}>Capacity</DescrTitle>
-              <DescrTitle isDarkTheme={isDarkTheme}>RAM</DescrTitle>
-            </DescrBox>
+      <DescrWrapper>
+        <DescrBox>
+          <DescrTitle isDarkTheme={isDarkTheme}>Screen</DescrTitle>
+          <DescrTitle isDarkTheme={isDarkTheme}>Capacity</DescrTitle>
+          <DescrTitle isDarkTheme={isDarkTheme}>RAM</DescrTitle>
+        </DescrBox>
 
-            <DescrBox>
-              <DescrValue isDarkTheme={isDarkTheme}>{phone.screen}</DescrValue>
-              <DescrValue isDarkTheme={isDarkTheme}>{phone.capacity}</DescrValue>
-              <DescrValue isDarkTheme={isDarkTheme}>{phone.ram}</DescrValue>
-            </DescrBox>
-          </DescrWrapper>
+        <DescrBox>
+          <DescrValue isDarkTheme={isDarkTheme}>{phone.screen}</DescrValue>
+          <DescrValue isDarkTheme={isDarkTheme}>{phone.capacity}</DescrValue>
+          <DescrValue isDarkTheme={isDarkTheme}>{phone.ram}</DescrValue>
+        </DescrBox>
+      </DescrWrapper>
 
-          <ButtonsWrapper>
-            <ButtonAdd
-              isDarkTheme={isDarkTheme}
-              onClick={(e) => {
-                toggleClick(phone, e);
-              }}
-              type="button"
-              isClicked={isSelected}
-            >
-              {isSelected ? 'Added' : 'Add to cart'}
-            </ButtonAdd>
-            <ButtonLike
-              isDarkTheme={isDarkTheme}
-              isClicked={!!isFavorite}
-              type="button"
-              // eslint-disable-next-line no-shadow
-              onClick={(e) => {
-                toggleFavorite(phone, e);
-              }}
-            >
-              <IconSprite />
-              {isFavorite && <Icon
-                spriteName="heart-field"
-                fill={theme.colors.accentSecondary}
-              />}
+      <ButtonsWrapper>
+        <ButtonAdd
+          isDarkTheme={isDarkTheme}
+          onClick={(e) => {
+            toggleClick(phone, e);
+          }}
+          type="button"
+          isClicked={isSelected}
+        >
+          {isSelected ? 'Added' : 'Add to cart'}
+        </ButtonAdd>
+        <ButtonLike
+          isDarkTheme={isDarkTheme}
+          isClicked={!!isFavorite}
+          type="button"
+          // eslint-disable-next-line no-shadow
+          onClick={(e) => {
+            toggleFavorite(phone, e);
+          }}
+        >
+          <IconSprite />
+          {isFavorite && <Icon
+            spriteName="heart-field"
+            fill={theme.colors.accentSecondary}
+          />}
 
-              {!isFavorite && (
-                <Icon
-                  spriteName="heart"
-                />
-              )}
+          {!isFavorite && (
+            <Icon
+              spriteName="heart"
+            />
+          )}
 
-              {isDarkTheme && isFavorite && <Icon
-                spriteName="heart-field"
-                fill={theme.colors.accentSecondary}
-              />}
+          {isDarkTheme && isFavorite && <Icon
+            spriteName="heart-field"
+            fill={theme.colors.accentSecondary}
+          />}
 
-              {isDarkTheme && !isFavorite && <Icon
-                spriteName="heart-white"
-              />}
-            </ButtonLike>
-          </ButtonsWrapper>
-        </>
-        // eslint-disable-next-line indent
-      )}
+          {isDarkTheme && !isFavorite && <Icon
+            spriteName="heart-white"
+          />}
+        </ButtonLike>
+      </ButtonsWrapper>
     </CardWrapper>
   );
 };
